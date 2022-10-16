11bd3375289e3e90b64c00a01516e18a91a6630d
1SergPav1 <pav220521@gmail.com>
2022-10-16 11:33:07 +0300

    DZ_5

---
+++ DZ_6.py
@@ -0,0 +1,25 @@
+# Задание № 1
+
+# st = 'ООООРРРРОРОРРРРРРРРООРОРОРРРРРРРРРРРРРРРРРРРРРРРРРРРРРРР'
+#
+# lst = st.split('О')
+# lst = list(filter(lambda x: 'Р' in x, lst))
+# try:
+#     print(len(max(lst)))
+# except:
+#     print('Решек нет')
+
+# Задание № 2
+
+# # задание № 3 из ДЗ 2
+# # Задайте список из n чисел последовательности (1 + 1/n) ** n и выведите на экран их сумму.
+# # старое решение
+# # num = int(input('Введи число: '))
+# # my_dict = {n: (1 + 1 / n) ** n for n in range(1, num + 1)}
+# # print(my_dict)
+# # print(sum(my_dict.values()))
+#
+# # новое решение
+# num = int(input('Введи число: '))
+# num_list = map((lambda x: (1 + 1 / x) ** x), range(1, num + 1))
+# print(sum(num_list))
